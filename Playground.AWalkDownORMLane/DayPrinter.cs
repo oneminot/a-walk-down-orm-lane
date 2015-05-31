bc0d8362981589d1cb112dfc8b1e0e1224a98fbf
kushal <kushaldeveloper@gmail.com>
2015-05-31 10:23:58 -0500

    add a class and a function print day to learn about refactoring a switch

---
+++ Playground.AWalkDownORMLane/DayPrinter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground.AWalkDownORMLane
+{
+    public class DayPrinter
+    {
+        public void PrintDayName()
+        {
+            switch (Name)
+            {
+                case DayName.Monday:
+                    break;
+                case DayName.Tuesday:
+                    break;
+                case DayName.Wednesday:
+                    break;
+                case DayName.Thursday:
+                    break;
+                case DayName.Friday:
+                    break;
+                case DayName.Saturday:
+                    break;
+                case DayName.Sunday:
+                    break;
+                default:
+                    break;
+            }
+        }
+        public enum DayName
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+
+        public DayName Name { get; set; }
+    }
+}
