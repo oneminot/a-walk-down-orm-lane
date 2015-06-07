927adf13b4fa364581364ecda2032127552bb160
kushal <kushaldeveloper@gmail.com>
2015-06-06 23:19:22 -0500

    create a failing test

---
+++ ConsoleTests.AWalkDownORMLane/PersonTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleTests.AWalkDownORMLane
+{
+    [TestClass]
+    public class PersonTests
+    {
+        [TestMethod]
+        public void TestPersonEmptyConstructor()
+        {
+            var person = new Person();
+            Assert.AreEqual(person.Name, "");
+        }
+    }
+}
